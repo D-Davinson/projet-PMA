cc2ec451a1229d27b389019e0c41634d6d53f3cd
Davinson <doglasprincedavinson@gmail.com>
2023-03-01 11:13:30 +0100

    first methods done

--- project/Biblio.py
+++ project/Biblio.py
@@ -9,19 +9,24 @@ class TaskSystem:
         self.lTask = lTask
         self.dict = dict
 
-    def getDependencies(self,nomTache):
-        visited = set()
-        deps = []
+    def getDependencies(self, nomTache):
+        depend = []
+        ensemble = set()
 
-        def search(t):
-            visited.add(t)
-            for depend in self.dict[t]:
-                if depend not in visited:
-                     search(depend)
-                     deps.append(depend)
+        for i in self.lTask:
+            if nomTache in i.reads:
+                ensemble.add(i.name)
+            if i.name not in ensemble and i.name != nomTache:
+                execution = True
+                for j in i.reads:
+                    if j in ensemble:
+                        execution = False
+                        break
+                if execution:
+                    depend.append(i.name)
 
-        search(nomTache)
-        return sorted(set(deps))
+        return depend
+
 
         
 # fonction
@@ -31,9 +36,12 @@ def runT1():
 def runT2():
     global Y
     Y = 2
+def runT3():
+    global W
+    W = 4
 def runTsomme():
-    global X, Y, Z
-    Z = X + Y
+    global X, Y, Z , W
+    Z = X + Y + W
 t1 = Task()
 t1.name = "T1"
 t1.writes = ["X"]
@@ -42,19 +50,21 @@ t2 = Task()
 t2.name = "T2"
 t2.writes = ["Y"]
 t2.run = runT2
+t3 = Task()
+t3.name = "T3"
+t3.writes = ["W"]
+t3.run = runT3
 tSomme = Task()
 tSomme.name = "somme"
-tSomme.reads = ["X", "Y"]
+tSomme.reads = ["X", "Y","W"]
 tSomme.writes = ["Z"]
 tSomme.run = runTsomme
 
-s1 = TaskSystem([t1, t2, tSomme], {"T1": [], "T2": ["T1"], "somme": ["T1", "T2"]})
+s1 = TaskSystem([t1, t2, tSomme,t3], {"T1": [], "T2": ["T1"], "somme": ["T1", "T2"],"T3":["somme","T2","T1"]})
 
 #compilaiton
 t1.run()
 t2.run()
+t3.run()
 tSomme.run()
-print(X)
-print(Y)
-print(Z)
-print(s1.getDependencies("somme"))
+print(s1.getDependencies("T3"))
